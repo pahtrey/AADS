f451e037f9cf49c5156796e02160af0931b7b650
Pahtrey <pahtrey@gmail.com>
2017-01-15 23:54:24 +0300

    Доделал лаб 2

--- lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2.cpp
@@ -5,6 +5,22 @@
 #include <fstream>
 
 /*
+В файле дерево в виде
+Ann
+.Bor
+..Fred
+..Pat
+...Stiv
+...Tom
+.Nina
+..John
+...Emily
+...Natali
+....Robert
+.....Zack
+..Jack
+.Den
+
 2. Ввести  произвольное  сильно  ветвящееся дерево.  Выдать
 списки вершин:
 1) являющихся листьями;
@@ -14,6 +30,10 @@
 */
 
 const int DL = 20;
+const int FATHER_LEVEL = 1;
+const int PROGRAMM_EXIT_CODE = 54;
+
+using namespace std;
 
 struct Tree
 {
@@ -53,8 +73,8 @@ int ReadFromFile(FILE *file, Tree **tree)
 		buffer[0] = '\0';
 
 		p->level = k;
-		p->leftChild = NULL;
-		p->rightChild = NULL;
+		p->leftChild = 0;
+		p->rightChild = 0;
 
 		if (k == 0) //корень
 		{
@@ -80,6 +100,8 @@ int ReadFromFile(FILE *file, Tree **tree)
 			{
 				q = q->father;
 			}
+			p->father = q->father;
+			q->rightChild = p;
 		}
 
 		m = k;
@@ -103,17 +125,139 @@ void BackFromBin(Tree *tree)
 
 		j = 0;
 
-		while (str[i++] = (tree->name)[j++])
-		{
-			printf("%s\n", str);
-		}
-
+		while (str[i++] = (tree->name)[j++]);
+		cout << str << endl;
 		BackFromBin(tree->leftChild);
 		BackFromBin(tree->rightChild);
 	}
 }
 
-int main()
+void printLeaves(Tree *tree)
 {
+	if (tree)
+	{
+		printLeaves(tree->leftChild);
+		if (!tree->leftChild && !tree->leftChild)
+		{
+			cout << tree->name << endl;
+		}
+		printLeaves(tree->rightChild);
+	}
+}
+
+void printNodes(Tree *tree)
+{
+	if (tree)
+	{
+		printNodes(tree->leftChild);
+		if (tree->leftChild || tree->leftChild)
+		{
+			cout << tree->name << endl;
+		}
+		printNodes(tree->rightChild);
+	}
+}
+
+void printLeavesFathers(Tree *tree)
+{
+	Tree *p;
+	p = new Tree;
+	if (tree)
+	{
+		printLeavesFathers(tree->leftChild);
+		if (!tree->leftChild && !tree->leftChild)
+		{
+			cout << tree->father->name << endl;
+		}
+		printLeavesFathers(tree->rightChild);
+	}
+}
+
+void printNodesByLevel(Tree *tree, int level)
+{
+	if (tree && tree->level <= level)
+	{
+		printNodesByLevel(tree->leftChild, level);
+		cout << tree->name << endl;
+		printNodesByLevel(tree->rightChild, level);
+	}
+}
+
+void ShowMenu()
+{
+	system("cls");
+	cout << "1. Вывод всего дерева" << endl;
+	cout << "2. Список вершин, являющихся листьями" << endl;
+	cout << "3. Список вершин, не являющихся листьями" << endl;
+	cout << "4. Список вершин, являющихся родителями листьев" << endl;
+	cout << "5. Список вершин заданного уровня, считая от корня" << endl;
+	cout << "6. Выход" << endl;
+}
+
+int main(int argc, char* argv[])
+{
+	Tree *root = 0;
+	FILE *file;
+	string name;
+	char ch, level;
+	setlocale(LC_ALL, "Rus");
+
+	if (argc != 2)
+	{
+		cout << "Неверное количество аргументов" << endl
+			<< "Использовать: lab2.exe <file>" << endl;
+		return 1;
+	}
+
+	file = fopen(argv[1], "r");
+
+	if (file == NULL)
+	{
+		cout << "Невозможно окрыть файл" << argv[1] << endl;
+		return 1;
+	}
+
+	ReadFromFile(file, &root);
+
+	ShowMenu();
+	do
+	{
+		ch = getchar();
+
+		switch (ch)
+		{
+		case 49:
+			ShowMenu();
+			cout << "Дерево:" << endl;
+			BackFromBin(root);
+			break;
+		case 50:
+			ShowMenu();
+			cout << "Список вершин, являющихся листьями:" << endl;
+			printLeaves(root);
+			break;
+		case 51:
+			ShowMenu();
+			cout << "Список вершин, не являющихся листьями:" << endl;
+			printNodes(root);
+			break;
+		case 52:
+			ShowMenu();
+			cout << "Список вершин, являющихся родителями листьев:" << endl;
+			printLeavesFathers(root);
+			break;
+		case 53:
+			ShowMenu();
+			cout << "Введите уровень:";
+			level = getchar();
+			level = getchar();
+			cout << "Список вершин заданного уровня, считая от корня:" << endl;
+			printNodesByLevel(root, (int) level - 48);
+			break;
+		default:
+			break;
+		}
+	} while (ch != PROGRAMM_EXIT_CODE);
+
     return 0;
 }
